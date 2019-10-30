0f0bd2dc2df4cded4a49278ba22c98cc4f686131
Matt Conway <mattconway727@gmail.com>
2019-10-29 20:22:13 -0400

    mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,36 +1,49 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./App.css";
 import StarwarsChars from "./components/StarwarsChars";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      starwarsChars: []
-    };
-  }
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       starwarsChars: []
+//     };
+//   }
 
-  componentDidMount() {
-    this.getCharacters("https://swapi.co/api/people/");
-  }
 
-  getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
-    fetch(URL)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  };
 
-  render() {
+
+
+  // componentDidMount() {
+  //   this.getCharacters("https://swapi.co/api/people/");
+  // }
+
+  // getCharacters = URL => {
+  //   // feel free to research what this code is doing.
+  //   // At a high level we are calling an API to fetch some starwars data from the open web.
+  //   // We then take that data and resolve it our state.
+  //   fetch(URL)
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       this.setState({ starwarsChars: data.results });
+  //     })
+  //     .catch(err => {
+  //       throw new Error(err);
+  //     });
+  // };
+      const App = () => {
+        const [starwarsChars, setStarwarsChars] = useState([]);
+        useEffect(() => {
+          axios.get("https://swapi.co/api/people/")
+          .then(res => {
+            console.log(res)
+            
+          })
+        },[])
+      }
+  
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
@@ -39,5 +52,7 @@ class App extends Component {
     );
   }
 }
+
+
 
 export default App;
